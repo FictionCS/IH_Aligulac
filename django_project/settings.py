7b8dcd781e64b333fd5e3e83b530f4022970eec8
unknown <Kevin Baker>
2021-02-16 13:49:51 -0800

    Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mek69m-esr52l4bzahzl+f1tron!!tdhwt+1y+pmjv_2#$ougt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ihaligulac.herokuapp.com']
 
 
 # Application definition
