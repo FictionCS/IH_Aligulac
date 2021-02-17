5b1b7e54d451bc718f473c67a84bb3e73b8e5293
unknown <Kevin Baker>
2021-02-16 16:45:21 -0800

    stuff

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+	path('', views.home, name='rankings-home')
     path('admin/', admin.site.urls),
     path('ranking/', include('rankings.urls')),
 ]
