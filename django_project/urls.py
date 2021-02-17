9e69879f5bb4799c6eb9253ca084afb7457560c7
unknown <Kevin Baker>
2021-02-16 17:06:25 -0800

    stuff

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ranking/', include('rankings.urls')),
+    path('', include('rankings.urls')),
 ]
