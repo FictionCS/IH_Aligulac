7851ae68cca41132931c6e5cea5dc4fd760c3ea7
unknown <Kevin Baker>
2021-02-17 19:26:39 -0800

    stuff

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
