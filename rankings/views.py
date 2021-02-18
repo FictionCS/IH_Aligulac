7b6654b337d720e8890357c22e29a505b5962feb
unknown <Kevin Baker>
2021-02-17 18:50:45 -0800

    stuff

--- rankings/views.py
+++ rankings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-from django_project.rankings.fullData import fullList
+from .fullData import fullList
 
 players = [
 	{
