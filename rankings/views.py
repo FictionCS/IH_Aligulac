ce401d03344cedc43210fcddbc7d458d316edb7c
unknown <Kevin Baker>
2021-02-23 15:34:02 -0800

    stuffs

--- rankings/views.py
+++ rankings/views.py
@@ -39,7 +39,7 @@ def player(request, player=''):
 		}
 	else:
 		context = {
-			'players':playerMatches[0]
+			'players':playerMatches.index(player)
 		}
 	return render(request, 'ALIGULAC/players.html', context)
 
