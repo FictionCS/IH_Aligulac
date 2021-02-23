c0364516129143338cad783138c6d10a0c797a73
unknown <Kevin Baker>
2021-02-23 12:54:43 -0800

    stuffs

--- rankings/urls.py
+++ rankings/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('matches/', views.matches, name='rankings-matches'),
     path('predictions/', views.predictions, name='rankings-predictions'),
     path('full/', views.full, name='rankings-full'),
-    path('about/', views.about, name='rankings-about')
-    path('about/page<int:num>/', views.about, name='rankings-about')
+    path('about/', views.about, name='rankings-about'),
+    url(r'about/(?P<username>.+)/$', views.about, name="profile")
 ]
