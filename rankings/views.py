d374f00cbfd312340528713510f9dc850b70297c
unknown <Kevin Baker>
2021-02-23 14:13:15 -0800

    stuffs

--- rankings/views.py
+++ rankings/views.py
@@ -37,7 +37,7 @@ def player(request, player=''):
 		'players':playerMatches
 	}
 	for player in range(len(playerMatches)):
-		if playerMatches[player][0] == player:
+		if playerMatches[player]['player'] == player:
 			context = {
 				'players':playerMatches[player]    
 			}
